927b780ae43b779b89474e823e814cd6ab6817be
Ben Muschol <muschol.b@husky.neu.edu>
2016-08-22 12:39:03 -0400

    extract laughter groups

--- app/pdf_parser.py
+++ app/pdf_parser.py
@@ -1,4 +1,5 @@
 import re
+import logging
 
 import slate
 
@@ -19,6 +20,8 @@ class PdfParser(object):
             "\s*1\s*2\s*3\s*4\s*5\s*6\s*7\s*8\s*9\s*10\s*11\s*12\s*13\s*14\s*15\s*16\s*17\s*18\s*19\s*20\s*21\s*22\s*23\s*24\s*25\s*",
             "Official\d+",
         ]
+    SPEAKER_PATTERN = "([A-Z][A-Z\s\.\\']+:)"
+    LAUGHTER = "(Laughter.)"
 
     def __init__(self, file_path):
         with open(file_path) as f:
@@ -29,11 +32,51 @@ class PdfParser(object):
             self.cleaned_text = re.sub(pattern, "", self.cleaned_text)
         self.cleaned_text = self.cleaned_text.replace(self.PAGE_BREAK, "")
 
-    def extract_lines(self):
-        cur_index = 0
+    def extract_laughter(self):
+        i = 0
         count = 0
-        num_laughs = self.cleaned_text.count("(Laughter.)")
+        num_laughs = self.cleaned_text.count(self.LAUGHTER)
 
-        #while count < num_laughs:
-        #    index = 
+        lines = self._zipped_lines()
+        laughter_groups = []
+
+        while count < num_laughs:
+            line = lines[i]
+            i += 1
+            if self.LAUGHTER in line[1]:
+                laughter_group = [lines[i-2], line]
+                count += 1
+                if len(lines) > i:
+                    next_line = lines[i]
+                    while self.LAUGHTER in next_line[1] and len(lines) > i:
+                        laughter_group += [next_line]
+                        count += 1
+                        i += 1
+                        next_line = lines[i]
+                    laughter_group += [next_line]
+                laughter_groups.append(laughter_group)
+        return laughter_groups
+
+
+    def _zipped_lines(self):
+        lines = re.split(self.SPEAKER_PATTERN, self.cleaned_text)
+        zipped = []
+        i = 0
+        cur_line = lines[i]
+        started_args = False
+
+        while 'submitted.)' not in cur_line:
+            try:
+                cur_line = lines[i]
+            except:
+                logging.log(logging.ERROR, 'Index %s out of bounds' % i)
+                break
+
+            i += 1
+            if not started_args:
+                started_args = 'proceedings' in cur_line.lower().replace(" ", "")
+            elif re.match(self.SPEAKER_PATTERN, cur_line) and len(lines) > i:
+                zipped.append([cur_line, lines[i].strip()])
+                i += 1
+        return zipped
 
